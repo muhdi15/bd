19a4a5db613cca71213f25917b89b9db30a55e98
muhdi15 <muhdimuhdi15@gmail.com>
2025-06-02 22:25:57 +0800

    Tugas3

--- tugas 2 Buat Document/playground-2.mongodb.js
+++ tugas 2 Buat Document/playground-2.mongodb.js
@@ -3,40 +3,5 @@ const database = "TransportationDB";
 const collection = "kendaraan";
 
 use(database);
-db.collection.insertOne({
-    "nama": "Kendaraan",
-    "jumlahRoda": 0,
-    "kapasitasPenumpang": 0,
-    "jenisBahanBakar": "Tidak Diketahui",
-    "tahunProduksi": 0
-});
-db.collection.insertMany([
-    {
-        "nama": "Bus",
-        "jumlahRoda": 6,
-        "kapasitasPenumpang": 50,
-        "jenisBahanBakar": "Solar",
-        "tahunProduksi": 2015
-    },
-    {
-        "nama": "Mobil",
-        "jumlahRoda": 4,
-        "kapasitasPenumpang": 5,
-        "jenisBahanBakar": "Bensin",
-        "tahunProduksi": 2020
-    },
-    {
-        "nama": "Sepeda Motor",
-        "jumlahRoda": 2,
-        "kapasitasPenumpang": 2,
-        "jenisBahanBakar": "Bensin",
-        "tahunProduksi": 2018
-    },
-    {
-        "nama": "Truk",
-        "jumlahRoda": 10,
-        "kapasitasPenumpang": 3,
-        "jenisBahanBakar": "Solar",
-        "tahunProduksi": 2017
-    }
-]);
+
+db.collection.find()
